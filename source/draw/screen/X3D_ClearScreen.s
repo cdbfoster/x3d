d0cc9a802a798ac0e140b208a9b83df5fd2f7abc
Chris Foster <cdbfoster@gmail.com>
2013-03-19 15:12:33 -0400

    Cleaned up the screen functions a bit.

--- source/draw/screen/X3D_ClearScreen.s
+++ source/draw/screen/X3D_ClearScreen.s
@@ -20,31 +20,34 @@
 .text
 .even
 
-.globl X3D_ClearScreen
+.global X3D_ClearScreen
 X3D_ClearScreen:
-	movem.l	%d3-%d7/%a2-%a5,-(%sp)
-	lea	(0xBB8,%a0),%a0
-	moveq	#0,%d0
-	moveq	#0,%d1
-	moveq	#0,%d2
-	moveq	#0,%d3
-	moveq	#0,%d4
-	moveq	#0,%d5
-	moveq	#0,%d6
-	movea.l	%d0,%a1
-	movea.l	%d0,%a2
-	movea.l	%d0,%a3
-	movea.l	%d0,%a4
-	movea.l	%d0,%a5
-	moveq	#14,%d7
+	movem.l	%d3 - %d7 / %a2 - %a5, -(%sp)
+	
+	lea	(3000, %a0), %a0
+	moveq	#0, %d0
+	moveq	#0, %d1
+	moveq	#0, %d2
+	moveq	#0, %d3
+	moveq	#0, %d4
+	moveq	#0, %d5
+	moveq	#0, %d6
+	movea.l	%d0, %a1
+	movea.l	%d0, %a2
+	movea.l	%d0, %a3
+	movea.l	%d0, %a4
+	movea.l	%d0, %a5
+	moveq	#14, %d7
 0:
-	movem.l	%d0-%d6/%a1-%a5,-(%a0)
-	movem.l	%d0-%d6/%a1-%a5,-(%a0)
-	movem.l	%d0-%d6/%a1-%a5,-(%a0)
-	movem.l	%d0-%d6/%a1-%a5,-(%a0)
-	dbf	%d7,0b
-	movem.l	%d0-%d6/%a1-%a5,-(%a0)
-	movem.l	%d0-%d6/%a1-%a5,-(%a0)
-	movem.l	%d0-%d5,-(%a0)
-	movem.l	(%sp)+,%d3-%d7/%a2-%a5
+	movem.l	%d0 - %d6 / %a1 - %a5, -(%a0)
+	movem.l	%d0 - %d6 / %a1 - %a5, -(%a0)
+	movem.l	%d0 - %d6 / %a1 - %a5, -(%a0)
+	movem.l	%d0 - %d6 / %a1 - %a5, -(%a0)
+	dbf	%d7, 0b
+	
+	movem.l	%d0 - %d6 / %a1 - %a5, -(%a0)
+	movem.l	%d0 - %d6 / %a1 - %a5, -(%a0)
+	movem.l	%d0 - %d5, -(%a0)
+	
+	movem.l	(%sp)+, %d3 - %d7 / %a2 - %a5
 	rts
--- source/draw/screen/X3D_CopyScreen.s
+++ source/draw/screen/X3D_CopyScreen.s
@@ -20,30 +20,32 @@
 .text
 .even
 
-.globl X3D_CopyScreen
+.global X3D_CopyScreen
 X3D_CopyScreen:
-	movem.l	%d3-%d7/%a2-%a6,-(%sp)
-	moveq	#9,%d7
+	movem.l	%d3 - %d7 / %a2 - %a6, -(%sp)
+	moveq	#9, %d7
 0:
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(%a1)
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(48,%a1)
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(96,%a1)
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(144,%a1)
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(192,%a1)
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(240,%a1)
-	lea	(%a1,288),%a1
-	dbf	%d7,0b
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(%a1)
-	movem.l	(%a0)+,%d0-%d6/%a2-%a6
-	movem.l	%d0-%d6/%a2-%a6,(48,%a1)
-	movem.l	(%a0)+,%d0-%d6
-	movem.l	%d0-%d6,(96,%a1)
-	movem.l	(%sp)+,%d3-%d7/%a2-%a6
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (%a1)
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (48, %a1)
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (96, %a1)
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (144, %a1)
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (192, %a1)
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (240, %a1)
+	lea	(288, %a1), %a1
+	dbf	%d7, 0b
+	
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (%a1)
+	movem.l	(%a0)+, %d0 - %d6 / %a2 - %a6
+	movem.l	%d0 - %d6 / %a2 - %a6, (48, %a1)
+	movem.l	(%a0)+, %d0 - %d6
+	movem.l	%d0 - %d6, (96, %a1)
+	
+	movem.l	(%sp)+, %d3 - %d7 / %a2 - %a6
 	rts

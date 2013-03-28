6e5dd7d2b4139b4725ef4506887d540461bba43c
Chris Foster <cdbfoster@gmail.com>
2013-03-28 01:43:26 -0400

    Added ScreenConstants in C and ASM for more centralized control of constants.

---
+++ source/system/ScreenConstants.s
@@ -0,0 +1,25 @@
+|	This file is part of X3D Engine.
+|
+|	X3D Engine is free software: you can redistribute it and/or modify
+|	it under the terms of the GNU Lesser General Public License as published by
+|	the Free Software Foundation, either version 3 of the License, or
+|	(at your option) any later version.
+|
+|	X3D Engine is distributed in the hope that it will be useful,
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|	GNU Lesser General Public License for more details.
+|
+|	You should have received a copy of the GNU Lesser General Public License
+|	along with X3D Engine.  If not, see <http://www.gnu.org/licenses/>.
+
+.set X3D_SCREEN_WIDTH,			160
+.set X3D_SCREEN_HEIGHT,			100
+.set X3D_SCREEN_MAX_X,			159
+.set X3D_SCREEN_MAX_Y,			99
+.set X3D_SCREEN_CENTER_X,		80
+.set X3D_SCREEN_CENTER_Y, 		50
+.set X3D_SCREEN_SCALE_X,		80
+.set X3D_SCREEN_SCALE_Y,		80
+.set X3D_SCREEN_BYTESPERLINE,	30
+.set X3D_SCREEN_WORDSPERLINE,	15

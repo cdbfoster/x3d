ca452ad0a51576aa9e639f009b155b291f5caa6c
Chris Foster <cdbfoster@gmail.com>
2013-04-19 15:38:23 -0400

    Added System.c to the build file.

--- source/system/build.bat
+++ source/system/build.bat
@@ -1,6 +1,9 @@
 @echo off
 tigcc EngineState.c ^
+      System.c ^
       -c -Os -Wall -W -Wwrite-strings -ffunction-sections -fdata-sections -DMIN_AMS=100
 tigcc EngineState.o ^
+      System.o ^
       -ar -o X3D_System -Os -Wall -W -Wwrite-strings -ffunction-sections -fdata-sections -DMIN_AMS=100
-echo Y | del EngineState.o
+echo Y | del EngineState.o ^
+      System.o
